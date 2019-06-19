bb7402415e1596a416ca6d902c24ac63a9b4b9db
erickslee <51988870+erickslee@users.noreply.github.com>
2019-06-19 16:26:31 +0800

    Added comment to explain the addition
    Code is efficient, but confusing

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,7 +12,7 @@ exports.calculate = function(req, res) {
 
   var operations = {
     // 2nd change by eric
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
